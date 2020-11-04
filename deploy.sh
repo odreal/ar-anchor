67fa59302e343345224044bc05842f580ea0264c
ODReal <honyxidradio@gmail.com>
2020-11-04 11:24:11 +0100

    dev

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-apt-get update && apt-get -y upgrade
-apt-get install git
-touch work
-
+apt-get -y install docker.io
+git clone https://github.com/xdspacelab/openvslam
+cd openvslam
+docker build -t openvslam-socket -f Dockerfile.socket .
+cd viewer
+docker build -t openvslam-server .
 node index.js
